28a69cb453f94859d7bbc30f9e59e057f55948ae
Samuel Etver <61624124+samuel-etver@users.noreply.github.com>
2022-01-28 15:04:32 +0500

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
 const fastify = require('fastify');
+
+const app = fastify({logger: true});
+
+app.get('/', function (req, reply) {
+    reply.send({hello: 'world'});
+});
+
+app.listen(3000, (err, address) => {
+    if (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+    app.log.info('');
+});
+
+console.log("TEST");
