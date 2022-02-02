8f1d652cec4e9cbb3766879bd315f3a89cf5fa84
Samuel Etver <61624124+samuel-etver@users.noreply.github.com>
2022-02-02 15:58:35 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,23 @@ app.get('/', function (req, reply) {
     reply.send({hello: 'world'});
 });
 
-app.listen(3000, (err, address) => {
+
+app.get('/ping', function (req, reply) {
+    console.log("asdasd");
+    reply.send({});
+});
+
+
+app.get('/rt-values', function (req, reply) {
+    reply.send({});
+});
+
+
+app.get('/rt-alerts', function (req, reply) {
+    reply.send({});
+});
+
+app.listen(8000, (err, address) => {
     if (err) {
         app.log.error(err);
         process.exit(1);
